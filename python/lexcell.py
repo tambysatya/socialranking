0061e2231622efc31ea843af35e969034fb05a03
satya tamby <tambysatya@gmail.com>
2023-05-07 15:55:07 +0200

    adv lexcel

--- python/lexcell.py
+++ python/lexcell.py
@@ -9,14 +9,13 @@ def nb_coalitions_ei_belongs_to (el, coals):
     return (len([1 for gi in coals[1] if el in gi]))    
 def nb_total_sorted_coalitions_ei_belongs_to (el, grouped_coals):
     #print (el, list(grouped_coals))
-    _,coals = itertools.tee(grouped_coals, 2)
-    return list(map (lambda gi: nb_coalitions_ei_belongs_to(el,gi), coals))
+    return list(map (lambda gi: nb_coalitions_ei_belongs_to(el,gi), grouped_coals))
 
     
 
 def lex_cell (individuals, coalitions, scores):
     #print (individuals)
-    sorted_coals = sorted (zip (scores, coalitions))
+    sorted_coals = sorted (zip (scores, coalitions), reverse=True)
 
     grouped_coals = itertools.groupby(sorted_coals, key=itemgetter(0))
     grouped_coals = map (lambda x: (x[0], list(map (itemgetter(1), x[1]))), grouped_coals) #group by list
@@ -42,6 +41,39 @@ def random_coalitions (individuals, l, n):
     """generates at most n coalitions of size n"""
     ret = [random_coalition(individuals,l) for i in range (n)]
     return [k for k, v in itertools.groupby(sorted(ret)) ]
+
+################## extended version
+
+def is_in_coal (el, coal): # Int -> ([{0,1}], [Int]) -> Bool
+    """ true if el belongs to the coalition AND is selected (ie ==1) """
+    pos, inds = coal
+    return el in inds and  pos[el] == 1
+
+def nb_coals_equal_1(el, coals):
+    return len([ci for ci in coals if is_in_coal(el, ci)])
+
+def compute_eq_classes (el, grouped_coals):
+    return list(map (lambda gi: nb_coals_equal_1(el,gi), grouped_coals))
+ 
+def adv_lex_cell (individuals, coalitions, scores):
+    #print (individuals)
+    sorted_coals = sorted (zip (scores, coalitions), reverse=True)
+
+    grouped_coals = itertools.groupby(sorted_coals, key=itemgetter(0))
+    grouped_coals = map (lambda x: (list(map (itemgetter(1), x[1]))), grouped_coals) #group by list
+    #print (sorted_coals)
+
+
+
+    lex_scores = map (lambda ei, gcoals: compute_eq_classes(ei, gcoals), individuals, itertools.tee(grouped_coals, len(individuals)))
+    lex_scores = sorted (zip(lex_scores, individuals), reverse=True)
+
+    #print (list (lex_scores))
+    lex_order = map (itemgetter(1), lex_scores)
+    
+    return list(lex_order)
+
+   
     
     
 
--- python/main.py
+++ python/main.py
@@ -1,6 +1,6 @@
 
 from MIP.kp import generateUniformKPND, generateWeaklyCorrelatedKPND, generateStronglyCorrelatedKPND, kp_greedy
-from lexcell import lex_cell, random_coalitions
+from lexcell import lex_cell, random_coalitions, adv_lex_cell
 from operator import itemgetter
 import random
 
@@ -25,7 +25,45 @@ import random
 #print (kp.display())
 #print (kp.greedy(order))
 
-n=300
+n=50
+def adv_test_kpnd(n_individuals, nd):
+    individuals = set(range(n_individuals))
+    kp = generateUniformKPND(n_individuals,1000, nd)
+    #kp = generateWeaklyCorrelatedKPND(n_individuals,1000, nd)
+    #kp = generateStronglyCorrelatedKPND(n_individuals,1000, nd)
+    coals = random_coalitions(individuals, 35,200)
+    print ("nb_coals=", len(coals))
+    scores,sols = zip(*(list (map (kp.solve_coalition, coals))))
+
+    order = adv_lex_cell(individuals, list(zip(sols,coals)), scores)
+
+    opt = kp.solve()[0]
+    adv_lex = kp.greedy(order)
+    order = adv_lex_cell(individuals,list (zip (sols,coals)), scores)
+    order.reverse()
+    adv_rev_lex = kp.greedy(order)
+
+    order = lex_cell(individuals,coals, scores)
+
+    opt = kp.solve()[0]
+    lex = kp.greedy(order)
+    order = lex_cell(individuals,coals, scores)
+    order.reverse()
+    rev_lex = kp.greedy(order)
+
+
+
+    rnd_order = list(range(n_individuals))
+    random.shuffle(rnd_order)
+    rnd = kp.greedy(rnd_order)
+
+
+    #real_greedy_order = kp_greedy(kp.objcoefs, kp.A[0])
+    #real_greedy = kp.greedy(real_greedy_order)
+    #print ("opt=",opt," adv_lex=", adv_lex, " lex=", lex, " adv_rev_lex=", adv_rev_lex, " rev_lex=",rev_lex, " rnd=", rnd, " real_greed=", real_greedy)
+    print ("opt=",opt," adv_lex=", adv_lex, " lex=", lex, " adv_rev_lex=", adv_rev_lex, " rev_lex=",rev_lex, " rnd=", rnd)
+
+
 def test_kpnd(n_individuals, nd):
     individuals = set(range(n_individuals))
     kp = generateStronglyCorrelatedKPND(n_individuals,1000, nd)
@@ -78,6 +116,6 @@ def test_kp(n_individuals):
     print ("opt=",opt," lex=", lex, " rev_lex=", rev_lex, " real=", real_greedy, " rnd=", rnd)
 
 for i in range(10):
-    test_kpnd(n, 5)
+    adv_test_kpnd(n, 10)
 
 
