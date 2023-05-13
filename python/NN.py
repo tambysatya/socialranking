9973409bce0b01fbe914495060e70eea3f6105f2
tamby <tambysatya@gmail.com>
2023-05-13 23:22:51 +0200

    test cross entrop

--- python/NN.py
+++ python/NN.py
@@ -35,21 +35,24 @@ class ResidualLinear (nn.Module):
         return x
 
 class TestNet (nn.Module):
-    def __init__ (self, bsize, objs, A, b): 
+    def __init__ (self, objs, A, b): 
         super (TestNet, self).__init__()
         
 
-        self.instance = torch.cat((objs,A.reshape(n*m),b)).repeat(bsize,1)
+        self.instance = torch.cat((objs,A.reshape(n*m),b))
         in_features = n+n*m+m+n
         self.linear1 = nn.Linear(n+n*m+m+n,hid, bias=True) #f(x) = ax+b
         self.b1 = ResidualLinear(hid)
         self.b2 = ResidualLinear(hid)
-        self.linear2 = nn.Linear(hid,n+1, bias = True)
+        self.linear2 = nn.Linear(hid,n, bias = True)
+        #self.linear2 = nn.Linear(hid,n+1, bias = True)
 
 
 
     def forward (self, x):
-        inputs = torch.cat((self.instance,x), dim=1)
+        self.train()
+        bsize = x.size()[0]
+        inputs = torch.cat((self.instance.repeat(bsize,1),x), dim=1)
         x = self.linear1(inputs)
         x = F.relu(x)
         x = self.b1(x)
@@ -57,12 +60,6 @@ class TestNet (nn.Module):
         y = self.linear2(x)
         return y
 
-
-    def infer (self, batch):
-        x, f_x = batch
-        y = self.forward(x)
-        loss = F.l1_loss(f_x, y)
-        return loss
 
    
 def plot_grads(model):
@@ -78,73 +75,64 @@ def train(model,epoch, batch_size,lossfunction=F.l1_loss):
     optimizer = optim.Adam(model.parameters(), lr=lr)
     writer = SummaryWriter(comment="n-100_p-10_rnd")
     dataset = torch.load("dataset.pt")
+    testset = torch.load("testset.pt")
    
     print ("loaded")
     dataset = DataLoader(dataset, batch_size = batch_size, shuffle=True)
+
+    testinput = testset[:,:n].to(device)
+    #testtarget = testset[:,n:].to(device)
+    #testtarget[:,n] = testtarget[:,n]/100000
+    testtarget = testset[:,n+1:].to(device)
     
     count=0
-    for _ in range(epoch):
+    for e in range(epoch):
         for batch in dataset:
+            optimizer.zero_grad()
             loss = 0
             # loss = model.learn(optimizer, dataset)
             #x, f_x = batch[:,0].reshape([batch_size, 1]), batch[:,1].reshape([batch_size,1])
             x = batch[:,:n]
             #opt=batch[:,n]
             #sol=batch[:,n+1:]
-            target=batch[:,n:].to(device)
-            target[:n] = target[:n]/100000
+
+            # full target
+            #target=batch[:,n:].to(device)
+            #target[:,n] = target[:,n]/100000
+
+            #only sol:
+            target = batch[:,n+1:].to(device)
 
             x = x.to(device)
             #opt = opt.to(device)
             #sol = sol.to(device)
             y = model.forward(x)
 
-            loss = lossfunction(target, y)
+            #loss = lossfunction(target, y)
+            loss = F.binary_cross_entropy_with_logits(y, target)
             loss.backward()
             optimizer.step() # un pas de la descente de gradient
 
             writer.add_scalar("loss/train", loss, count)
             writer.add_scalar("grad/norm", plot_grads(model), count)
             count += 1
+        with torch.no_grad():
+            output = model.forward(testinput)
+            loss = F.binary_cross_entropy_with_logits(output, testtarget)
+            #loss = lossfunction(testtarget, output)
+            writer.add_scalar("loss/test", loss, e)
+
+            output = torch.sigmoid(output)
+            output[output>0.5]=1
+            output[output<=0.5]=0
+            #print (output[0])
+            writer.add_scalar("loss/accuracy", (n - (output-testtarget).abs().sum(dim=1)).mean(), e)
+        
 
 
 def show_params(model):
     for param in model.parameters():
         print (param)
-
-def auto_gpu_selection(usage_max=0.01, mem_max=0.05):
-#Auto set CUDA_VISIBLE_DEVICES for gpu
-#:param mem_max: max percentage of GPU utility
-#:param usage_max: max percentage of GPU memory
-#:return:
-
- device = "cpu"
- os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
- if (not os.path.exists("/usr/bin/nvidia-smi")) : return device
- log = str(subprocess.check_output("nvidia-smi", shell=True)).split(r"\n")[6:-1]
- gpu = 0
-
- for i in range(len(tf.config.list_physical_devices('GPU'))):
-    idx = i*4 + 3   
-    if idx > log.__len__()-1:
-        break
-    inf = log[idx].split("|")
-    if inf.__len__() < 3:
-        break
-    usage = int(inf[3].split("%")[0].strip())
-    mem_now = int(str(inf[2].split("/")[0]).strip()[:-3])
-    mem_all = int(str(inf[2].split("/")[1]).strip()[:-3])
-    # print("GPU-%d : Usage:[%d%%]" % (gpu, usage))
-    if usage < 100*usage_max and mem_now < mem_max*mem_all:
-        device = "cuda:" + str(gpu)
-        print("\nAuto choosing vacant GPU-%d : Memory:[%dMiB/%dMiB] , GPU-Util:[%d%%]\n" %  (gpu, mem_now, mem_all, usage))
-        return device
-    #print("GPU-%d is busy: Memory:[%dMiB/%dMiB] , GPU-Util:[%d%%]" %
-     #     (gpu, mem_now, mem_all, usage))
-    gpu += 1
-
- print("\nNo vacant GPU, use CPU instead\n")
- return device # set to cpu
 
         
 if __name__ == '__main__':
@@ -169,7 +157,7 @@ if __name__ == '__main__':
     b = torch.load("b.pt").to(device)
  
     
-    model = TestNet(batchsize,objs/1000,A/1000,b/100000).to(device)    
+    model = TestNet(objs/1000,A/1000,b/100000).to(device)    
     print("before training")
     #show_params(model)
     #train(5,int(1e7))
